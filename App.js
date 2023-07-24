a930a4e75fafb0797ab9f878ba4585a2668ddb55
andremugabo <andremugabo@yahoo.fr>
2023-07-24 20:32:19 +0200

    fourth commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Callout, Marker, Circle } from "react-native-maps";
 import { StyleSheet, View, Text, Button, Dimensions } from "react-native";
 import * as Location from "expo-location";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default function App() {
   const [mapRegion, setMapRegion] = useState({
@@ -9,6 +10,10 @@ export default function App() {
     longitude: -122.4324,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
+  });
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
   });
   const userLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -33,10 +38,70 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        fetchDetails={true}
+        GooglePlacesSearchQuery={{ rankby: "distance" }}
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+          setRegion({
+            latitude: details.geometry.location.lat,
+            longitude: details.location.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+        }}
+        query={{
+          key: "AIzaSyC6BUDJwedjveLT5n8vCeouK11rM7Aq3ME",
+          language: "en",
+          components: "country:rw",
+          types: "establishment",
+          radius: 3000,
+          location: `${region.latitude},${region.longitude}`,
+        }}
+        styles={{
+          container: {
+            flex: 0,
+            position: "absolute",
+            width: "100%",
+            zIndex: 1,
+          },
+          listView: { backgroundColor: "white" },
+        }}
+      />
       <MapView style={styles.map} region={mapRegion} zoomEnabled={true}>
-        <Marker coordinate={mapRegion} title="Marker" />
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
+        />
+        <Marker
+          coordinate={mapRegion}
+          title="Marker"
+          pinColor="black"
+          draggable={true}
+          onDragStart={(e) => {
+            console.log("Drag start", e.nativeEvent.coordinate);
+          }}
+          onDragEnd={(e) => {
+            setMapRegion({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+          }}
+          // provider="google"
+        >
+          <Callout>
+            <Text>I'm here</Text>
+          </Callout>
+        </Marker>
+        <Circle center={mapRegion} radius={1000} />
       </MapView>
-      <Button title="Get Location" onPress={userLocation} />
+      <View>
+        <Button title="Get Location" onPress={userLocation} />
+      </View>
     </View>
   );
 }
@@ -44,9 +109,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    marginTop: 50,
+    // marginVertical: 100,
   },
   map: {
     width: Dimensions.get("window").width,
